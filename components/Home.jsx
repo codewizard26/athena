55a8bb65447c0109bd65e942de3b894944d7633e
Harsh Mittal <harshmittal1750@gmail.com>
2024-03-15 23:45:20 +0530

    Add Google Fonts import and update UI layout in globals.css and Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import React from 'react'
-import { Button } from './ui/button'
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
 
 function Home() {
   return (
     <div>
-        <div className="moon-container absolute top-0 left-1/2 transform -translate-x-1/2">
+      <div className="moon-container absolute top-0 left-1/2 transform -translate-x-1/2">
         <div className="moon-section flex flex-row justify-center">
           <Image
             className="moon-image"
@@ -16,22 +16,28 @@ function Home() {
           />
         </div>
       </div>
-        <div className="grid grid-cols-2">
-          <div className="h-full w-full sm:space-y-6 flex flex-col justify-center">
-            <div className=" md:text-7xl font-[550]">
-              <div className="gradient-text">Where Ideas</div>
-              <div className="gradient-text">Become Reality</div>
-            </div>
-            <div className="flex justify-start">
-              <Button>Free Consultation</Button>
-            </div>
+      <div className="grid grid-cols-2">
+        <div className="h-full w-full sm:space-y-6 flex flex-col justify-center">
+          <div className="text-xl md:text-4xl lg:text-6xl xl:text-7xl  font-[550]">
+            <div className="gradient-text">Where Ideas</div>
+            <div className="gradient-text">Become Reality</div>
           </div>
-          <div className="">
-            <Image height='600' width="600" className='moorti-1' src="moorti.svg" alt="moorti" />
+          <div className="flex justify-start">
+            <Button>Free Consultation</Button>
           </div>
         </div>
+        <div className="">
+          <Image
+            height="600"
+            width="600"
+            className="moorti-1"
+            src="moorti.svg"
+            alt="moorti"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
